c0e6e1892c0beb922ba993385593989ccb8df238
社会易姐QwQ <45892418+SocialSisterYi@users.noreply.github.com>
2022-05-09 11:30:33 +0800

    init

---
+++ Plugin.php
@@ -0,0 +1,164 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * 评论区显示 ip 及归属地的插件 (使用<a href="https://www.ipip.net/product/ip.html">ipip.net Free库</a>, 以及<a href="https://github.com/ipipdotnet/ipdb-php">ipdb for PHP)</a>
+ * 
+ * @package CommentShowIp
+ * @author 社会易姐QwQ
+ * @version 0.0.1
+ * @link https://shakaianee.top
+ */
+class CommentShowIp_Plugin implements Typecho_Plugin_Interface {
+    /**
+     * 激活插件方法
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate() {
+        Typecho_Plugin::factory('Widget_Abstract_Comments')->contentEx = ['CommentShowIp_Plugin', 'replyHook'];
+    }
+
+    /**
+     * 禁用插件方法
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate() {
+    }
+    
+    /**
+     * 插件配置方法
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form) {
+        $mode = new Typecho_Widget_Helper_Form_Element_Checkbox(
+            'mode',
+            [
+                'hook' => '以hook模式显示ip  (不推荐使用)',
+            ],
+            NULL,
+            _t('插件模式设置'),
+            _t('Hook模式指使用contentEx位点添加ip信息到评论正文前<br>缺点是无法自定义样式')
+        );
+        $ipShow = new Typecho_Widget_Helper_Form_Element_Checkbox(
+            'ip_show',
+            [
+                'mask' => 'ip后16位打码',
+                'city' => '归属地精确到城市',
+            ],
+            ['mask', 'city'],
+            _t('ip展示设置')
+        );
+        $form->addInput($mode);
+        $form->addInput($ipShow);
+    }
+    
+    /**
+     * 个人用户的配置方法
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+
+    /**
+     * 获取ip归属地
+     *
+     * @access private
+     * @param string $ip
+     * @return string
+     */
+    private static function getIpLoc($ip) {
+        require_once('ipip/db/City.php');
+        require_once('ipip/db/CityInfo.php');
+        require_once('ipip/db/Reader.php');
+
+        $config = Typecho_Widget::widget('Widget_Options')->Plugin('CommentShowIp');
+        $dir = dirname(__file__);
+        $ipdbPath = $dir.'/ipipfree.ipdb';
+
+        try {
+            $ipdb = new \ipip\db\City($ipdbPath); // 加载ipip.net库
+            $city = $ipdb->findInfo($ip, 'CN');
+            if($city->country_name != '中国')
+                $loc = $city->country_name;
+            else {
+                $loc = $city->region_name;
+                if(in_array('city', ($config->ip_show) ? $config->ip_show : []))
+                    $loc .= $city->city_name;
+            }
+        } catch(Exception $e) {
+            $loc = '获取错误';
+        }
+        return $loc;
+    }
+
+    /**
+     * 为真实ip打码
+     *
+     * @access private
+     * @param string $ip
+     * @return string
+     */
+    private static function maskIp($ip) {
+        $config = Typecho_Widget::widget('Widget_Options')->Plugin('CommentShowIp');
+        if(in_array('mask', ($config->ip_show) ? $config->ip_show : []))
+            $ip = join('.', array_slice(explode('.', $ip), 0, 2)).'.*.*';
+        return $ip;
+    }
+
+    /**
+     * 输出评论区ip
+     *
+     * @access public
+     * @param Widget_Abstract_Comments $reply
+     * @param string $template
+     * @param int $type
+     * @return string
+     */
+    public static function output($reply, $template = NULL, $type = 0) {
+        $loc = self::getIpLoc($reply->ip);
+        $ip = self::maskIp($reply->ip);
+        if($template == NULL) {
+            $template = "ip属地: {loc} ({ip})<br>";
+        }
+        $content = str_replace(['{ip}', '{loc}'], [$ip, $loc], $template);
+        if($type == 1)
+            return $content;
+        else
+            echo $content;
+    }
+
+    /**
+     * 使用Hook模式显示评论区ip
+     *
+     * @access public
+     * @param string $template
+     * @param Widget_Abstract_Comments $reply
+     * @return string
+     */
+    public static function replyHook($text, $reply) {
+        $config = Typecho_Widget::widget('Widget_Options')->Plugin('CommentShowIp');
+        if(
+            in_array('hook', ($config->mode) ? $config->mode : []) &&
+            !(
+                $reply instanceof Widget\Comments\Admin || 
+                $reply instanceof Widget\Comments\Edit || 
+                $reply instanceof Widget\Comments\Ping ||
+                $reply instanceof Widget\Comments\Recent
+            )
+        )
+            return $reply->autoP(self::output($reply, NULL, 1)).$text;
+        else
+            return $text;
+    }
+}
---
+++ README.md
@@ -0,0 +1,65 @@
+# Typecho-Plugin-CommentShowIp
+
+Tpecho 的评论区显示评论发布者 ip 及归属地插件
+
+## 简介
+
+为博客评论区留言增加 ip 及归属地显示，用以确定发布者身份及防止“地域黑”行为
+
+> 2021年10月26日，国家网信办发布《互联网用户账号名称信息管理规定（征求意见稿）》（以下简称“《征求意见稿》”），拟要求平台在用户账号信息页面展示账号IP地址属地信息：境内用户账号IP地址属地信息需标注到省（区、市），境外账号IP地址属地信息需标注到国家（地区）。
+
+以上内容与本项目无实际关联，仅作为整活使用（手动滑稽
+
+## 安装
+
+将本项目文件夹放入 /usr/plugins 下，并在后台的插件管理中启用插件
+
+## 插件配置
+
+- 开启以hook模式显示ip：见下文介绍
+- ip后16位打码：模板输出的`{ip}`信息是否后16bit使用\*表示，如：192.168.\*.\*
+- 归属地精确到城市：模板输出的`{loc}`信息是否精确到地级市，如：广东广州
+
+## 使用方法
+
+### Hook模式
+
+将插件配置中的`以hook模式显示ip`复选框选中，发布者 ip 将自动输出至评论正文区域的第一行
+
+默认关闭，不推荐使用，使用该方法无法自定义ip显示样式和输出位点
+
+eg：使用默认模板
+
+![](http://i0.hdslb.com/bfs/album/048aa4afb3531c83346574ed93988788bdc38f66.png)
+
+### API模式
+
+使用`CommentShowIp_Plugin::output($reply, $template = NULL, $type = 0)`函数进行调用
+
+参数说明：
+
+| 参数     | 说明           | 类型                     | 是否可选 | 说明                                                         |
+| -------- | -------------- | ------------------------ | -------- | ------------------------------------------------------------ |
+| reply    | 评论View层对象 | Widget_Abstract_Comments | ×        |                                                              |
+| template | 模板           | string \|\| NULL         | √        | 输出渲染模板，如为NULL，则使用内建样式<br />格式化符`{ip}`：ip地址信息<br />格式化符`{loc}`：归属地信息 |
+| type     | 输出模式       | int                      | √        | 0：使用`echo`输出到网页<br />1：使用`return`返回             |
+
+eg：使用 MDUI 框架
+
+在合适位置加入以下代码（`$this`为实例化的评论区View层对象）
+
+```php
+CommentShowIp_Plugin::output($this, "
+<div>
+    <span style=\"font-size:10px;\" mdui-tooltip=\"{content: '{ip}'}\">ip属地: {loc}</span>
+</div>"
+);
+```
+
+![](http://i0.hdslb.com/bfs/album/c53eaa826316400cd1c4ef0d2c912768878e961f.png)
+
+## 声明
+
+插件使用[ipip.net Free库](https://www.ipip.net/product/ip.html)以及[ipdb for PHP Library](https://github.com/ipipdotnet/ipdb-php)
+
+商业使用禁止
